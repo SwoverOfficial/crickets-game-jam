26608aa3e1498386bcdb11a4a682716de43b3599
SwoverOfficial <swoverofficial@gmail.com>
2025-02-27 19:35:45 -0500

    Teleportation
    +Added first perception box area
    +Added teleportation system, clicking on paintings to teleport to other boxes
    +Fixed HUD text to fit text in one line

---
+++ Untitled Crickets Game/Assets/Scripts/Player/TeleportPlayer.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportPlayer : MonoBehaviour
+{
+    private CharacterController cc;
+
+    private void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+    public void TeleportPlayerToArea(Transform spawnTransform)
+    {
+        cc.enabled = false; //Turning off character controller gets rid of glitch where player "rubberbands" back and doesn't teleport
+        transform.position = spawnTransform.position;
+        transform.rotation = spawnTransform.rotation;
+        cc.enabled = true;
+    }
+}
