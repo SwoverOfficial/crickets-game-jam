8a15abc396c8bb33fc6e67d5417b2445e18c96b2
SwoverOfficial <swoverofficial@gmail.com>
2025-02-28 19:00:33 -0500

    Item Pick Ups
    +Added item pick up system, can carry and drop items now

---
+++ Untitled Crickets Game/Assets/Scripts/Objects/PickUpController.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpController : MonoBehaviour
+{
+    [SerializeField] private SpriteBillboard spriteBillboardScript;
+    
+    public static bool slotFull;
+    private bool pickedUp;
+
+    public GameObject playerItemSlot;
+    public GameObject parentObject;
+    public GameObject spriteChildObject;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (slotFull && pickedUp && Input.GetKeyDown(KeyCode.Q))
+            Drop();
+    }
+
+    public void PickUp()
+    {
+        if (slotFull)
+            return;
+
+        slotFull = true;
+        pickedUp = true;
+
+        spriteBillboardScript.enabled = false;
+
+        transform.SetParent(playerItemSlot.transform);
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.Euler(Vector3.zero);
+        spriteChildObject.transform.localRotation = Quaternion.Euler(Vector3.zero);
+    }
+
+    public void Drop()
+    {
+        slotFull = false;
+        pickedUp = false;
+
+        transform.SetParent(parentObject.transform);
+
+        transform.localRotation = Quaternion.Euler(Vector3.zero);
+        spriteChildObject.transform.localRotation = Quaternion.Euler(Vector3.zero);
+
+        spriteBillboardScript.enabled = true;
+    }
+}
